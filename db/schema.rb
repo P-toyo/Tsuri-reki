d747d90134f81696ad063229df92f1bcf6012ee7
P-toyo <whitegraduation1095winter@gmail.com>
2022-11-15 06:48:28 +0000

    投稿の公開非公開設定追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_023935) do
+ActiveRecord::Schema.define(version: 2022_11_15_052239) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2022_11_15_023935) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "prefecture_id"
+    t.boolean "status", default: true, null: false
   end
 
   create_table "prefectures", force: :cascade do |t|
