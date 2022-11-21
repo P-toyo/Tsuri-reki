1248d6a40d59e4029fc74d1d6b622754ae327b5d
P-toyo <whitegraduation1095winter@gmail.com>
2022-11-21 15:47:41 +0000

    釣り場詳細にグーグルマップを表示

--- app/controllers/public/areas_controller.rb
+++ app/controllers/public/areas_controller.rb
@@ -3,6 +3,11 @@ class Public::AreasController < ApplicationController
   def show
     @area = Area.find(params[:id])
     @area_rating = AreaRating.new
+    species_ids = Post.where(area_id: @area.id).pluck(:species_id).uniq
+    @species_name = Species.where(id: species_ids).pluck(:name)
+    @catch_number =[]
+    species_ids.each do |species_id|
+      @catch_number << @area.posts.where(species_id: species_id).sum(:catch_number)
+    end
   end
-
 end
