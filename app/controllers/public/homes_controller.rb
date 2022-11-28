8b43e48a47dc2dbe6ff41eef6381c6b3a04f9703
P-toyo <whitegraduation1095winter@gmail.com>
2022-11-28 14:58:16 +0000

    レイアウトの修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
     @regions = Region.all
-    @posts = Post.order(created_at: :desc).limit(5)
+    @posts = Post.order(created_at: :desc).limit(3)
   end
 end
