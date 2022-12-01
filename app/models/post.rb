b771b4f9c9ef04cdaa4373a236fbef1fff1c39a1
P-toyo <whitegraduation1095winter@gmail.com>
2022-12-01 08:39:53 +0000

    画像認識機能の追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :species
   belongs_to :fishing_method
   belongs_to :time_zone
+  has_many :image_tags, dependent: :destroy
   has_many   :post_tags, dependent: :destroy
   has_many   :prefectures, through: :post_tags
   has_many   :comments, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_24_075401) do
+ActiveRecord::Schema.define(version: 2022_12_01_074913) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,13 @@ ActiveRecord::Schema.define(version: 2022_11_24_075401) do
   end
 
   create_table "fishing_records", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "image_tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
