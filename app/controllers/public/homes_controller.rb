bfc78f3a5996cde10dacdc60c86015c261fbd552
P-toyo <whitegraduation1095winter@gmail.com>
2022-12-13 14:54:20 +0000

    お知らせ欄の実装中

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @regions = Region.all
+    @information = Information.order(created_at: :desc).limit(3)
     @posts = Post.order(created_at: :desc).limit(3)
   end
 end
