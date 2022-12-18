7ac0228a5dce0f77566618159cc0f3cb0314fd88
P-toyo <whitegraduation1095winter@gmail.com>
2022-12-18 13:44:23 +0000

    アラートの追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -37,7 +37,7 @@ class Public::PostsController < ApplicationController
       end
       redirect_to post_path(@post.id)
     else
-      flash[:alert] = "全て入力してください"
+      flash[:alert] = "その他釣果、コメント以外全て入力してください"
       @prefecture_id = @post.prefecture_id
       render action: :new
     end
